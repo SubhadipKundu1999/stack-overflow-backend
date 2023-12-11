233a9f84b3d98548afabeed76a024d13b3c0b8d2
Subhadip Kundu <subhadipkundu1000@gmail.com>
2023-12-11 23:53:03 +0530

    upload profile picture, trying to add google authentication and bad word detection(pending)

--- controllers/answer.js
+++ controllers/answer.js
@@ -1,25 +1,88 @@
 import Questions from "../model/questionModel.js";
 import mongoose from "mongoose";
+import { clean } from "../utils.js";
+import fetch from 'node-fetch'
+import {Headers} from 'node-fetch';
+// export const postAnswer = async (req,res)=>{
+//     const {id:id} = req.params;
+//     let {noOfAnswer, answerBody, userAnswered,userId} = req.body;
+//     if(!mongoose.Types.ObjectId.isValid(id)){                
+//         res.status(404).json({message:"Question not valid"});
+//     }
+//     updateNoOfAnswer(id,noOfAnswer);
+//     try{
+        
+//       let myHeaders = new Headers();
+//       myHeaders.append("apikey", "QUpHXk6DpE60IVivYIwt3wWmtb8Hs6m4");
+  
+//       let requestOptions = {
+//         method: 'POST',
+//         redirect: 'follow',
+//         headers: myHeaders,
+//         body: answerBody // Use the original answerBody here, not raw
+//       };
+//       answerBody = "what the fuck";
+//       console.log(answerBody);
+//       const response = await fetch("https://api.apilayer.com/bad_words?censor_character={censor_character}", requestOptions);
+//       const result = await response.json();
+//       answerBody =await  result.censored_content; // Update answerBody with the cleaned content
+//      console.log(answerBody);
+//         const updatedQuestion = await Questions.findByIdAndUpdate(id,{$addToSet:{'answer':{answerBody, userAnswered, userId}}} );
+//        res.status(200).json(updatedQuestion); 
+//      }
+//      catch(error){
+//         console.log(error);
+//         res.status(500).json("something went wrong");
 
-export const postAnswer = async (req,res)=>{
-    const {id:id} = req.params;
-    const {noOfAnswer, answerBody, userAnswered,userId} = req.body;
+//     }
+// }
 
-    if(!mongoose.Types.ObjectId.isValid(id)){                
-        res.status(404).json({message:"Question not valid"});
+export const postAnswer = async (req, res) => {
+  const { id: id } = req.params;
+  let { noOfAnswer, answerBody, userAnswered, userId } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "Question not valid" });
+  }
+
+  // Assuming updateNoOfAnswer is synchronous, if not, use await appropriately
+  updateNoOfAnswer(id, noOfAnswer);
+   console.log(answerBody);
+   
+  try {
+    let myHeaders = new Headers({
+        "apikey": "QUpHXk6DpE60IVivYIwt3wWmtb8Hs6m4"
+    });
+    answerBody= answerBody
+    console.log(answerBody);
+    let requestOptions = {
+      method: 'POST',
+      redirect: 'follow',
+      headers: myHeaders,
+      body:JSON.stringify(answerBody)
+    };
+    const response = await fetch("https://api.apilayer.com/bad_words?censor_character={censor_character}", requestOptions);
+
+    if (!response.ok) {
+      console.error(`API request failed with status: ${response.status}`);
+      const errorBody = await response.text();
+      console.error(`Error response body: ${errorBody}`);
+      throw new Error(`API request failed with status: ${response.status}`);
     }
-    updateNoOfAnswer(id,noOfAnswer);
-    try{
-        const updatedQuestion = await Questions.findByIdAndUpdate(id,{$addToSet:{'answer':{answerBody, userAnswered, userId}}} );
-       res.status(200).json(updatedQuestion); 
 
-     }
-     catch(error){
-        console.log(error);
-        res.status(500).json("something went wrong");
+    const result = await response.json();
+    answerBody = await result.censored_content;
+   let resultt= answerBody[-10];
+  console.log(typeof(resultt));
+    console.log(resultt);
 
-    }
-}
+    const updatedQuestion = await Questions.findByIdAndUpdate(id, { $addToSet: { 'answer': { answerBody, userAnswered, userId } } });
+    res.status(200).json(updatedQuestion);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("something went wrong");
+  }
+};
 
 
 export const deleteAnswer=async (req,res)=>{
--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const app = express();
 // 
 app.use(express.json({extended:true}));      
 app.use(express.urlencoded({ extended:true}))  
-app.use(cors());  
-
+app.use(cors()); 
 dotenv.config();
 
 const port =process.env.PORT || 5000
